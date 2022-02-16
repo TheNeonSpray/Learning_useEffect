90b518a78f139458e32d6e40cce0e72498b6f7ce
TheNeonSpray <alexandraeb20@gmail.com>
2022-02-16 11:46:18 -0600

    practice of useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+//useState is a function or "Hook" that handles and updates the state inside of a component
+import { useState, useEffect } from "react";
 
 function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [number, setNumber] = useState(0);
+
+  //you tell React that your component needs to do something after render.
+
+  /*componentDidUpdate
+  (everytime that I want to re render)*/
+  useEffect(() => {
+    console.log("didUpdate Effect");
+  });
+
+  /*componentDidMount
+  (when I want to call an API, or just a one time action)*/
+  useEffect(() => {
+    console.log("didMount Effect");
+    //when this component stops their tasks, this 'return' will unmount the component. (clear intervals, timers, listeners)
+    return()=>{
+      console.log("willUnmount");
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log("The input value or number has changed");
+  }, [inputValue, number]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={({ target: { value } }) => setInputValue(value)}
+      />
+      <input
+        type="text"
+        age={number}
+        onChange={({ target: { value } }) => setNumber(value)}
+      />
     </div>
   );
 }
